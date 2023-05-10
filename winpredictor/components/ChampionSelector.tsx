f7a46f0769fcc29c22daf502cc96c4828297742b
Nemika-Haj <mmnemijus@gmail.com>
2023-05-10 19:55:56 +0300

    style: added champion selector

---
+++ winpredictor/components/ChampionSelector.tsx
@@ -0,0 +1,73 @@
+import { Box, Flex, Image, Text, useToast } from "@chakra-ui/react";
+import champions from "../src/champions.json"
+
+type ChampionSelectorProps = {
+    filter: string
+    selectedChampions: string[]
+    setSelectedChampions: (champions: string[]) => void
+}
+
+export default function ChampionSelector(props: ChampionSelectorProps) {
+    const toast = useToast({
+        position: "top-right",
+        status: "success",
+        isClosable: true
+    })
+
+    return (
+        <Flex
+            marginTop={5}
+            wrap="wrap"
+            gap={3}
+            paddingX={0}
+            overflowY={"auto"}
+            minHeight="400px"
+            height="75vh"
+        >
+
+            {
+                Object.keys(champions)
+                    .filter(champion => champion.replace(" ", "").toLowerCase().includes(props.filter.toLowerCase().replace(" ", "")))
+                    .map(champion => {
+                        // @ts-ignore
+                        const champSrc = champions[champion]
+                        const classList = ["champion-icon-select"]
+
+                        if (props.selectedChampions.includes(champion)) classList.push("selected")
+
+                        return (
+                            <Box
+                                backgroundImage={`/assets/champions/${champSrc}`}
+                                key={`select-champion-${champion}`}
+                                className={classList.join(" ")}
+                                cursor="pointer"
+                                onClick={() => {
+                                    if(props.selectedChampions.includes(champion)) {
+                                        props.setSelectedChampions(props.selectedChampions.filter(champ => champ != champion))
+                                        toast({
+                                            description: <Text>Removed <strong>{champion}</strong> from the team builder!</Text>
+                                        })
+                                    }
+                                    else {
+                                        if(props.selectedChampions.length < 10) {
+                                            props.setSelectedChampions([...props.selectedChampions, champion])
+                                            toast({
+                                                description: <Text>Added <strong>{champion}</strong> to the team builder!</Text>
+                                            })
+                                        }
+                                        else {
+                                            toast({
+                                                status: "warning",
+                                                description: "You can only choose 10 champions!"
+                                            })
+                                        }
+                                    }
+                                }}
+                            />
+                        )
+                    })
+            }
+
+        </Flex>
+    )
+}
--- winpredictor/pages/index.tsx
+++ winpredictor/pages/index.tsx
@@ -1,10 +1,56 @@
+import ChampionSelector from "@/components/ChampionSelector";
 import BaseLayout from "@/components/layouts/BaseLayout";
+import { Grid, GridItem, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Index() {
-  
+  const [filter, setFilter] = useState<string>("")
+  const [selectedChampions, setSelectedChampions] = useState<string[]>([])
+
   return (
     <BaseLayout>
-      
+      <Grid
+        templateColumns={"repeat(10, 1fr)"}
+        templateRows={"repeat(1, 1f)"}
+        gap={5}
+        padding={5}
+      >
+
+        <GridItem
+          colSpan={2}
+          rowSpan={1}
+        >
+          a
+        </GridItem>
+
+        <GridItem
+          colSpan={6}
+          rowSpan={1}
+          background="gray.700"
+          padding={5}
+        >
+          <Input
+            variant="flushed"
+            background="gray.200"
+            color="gray.900"
+            paddingX="10px"
+            placeholder="Search for champions..."
+            _placeholder={{
+              color: "blackAlpha.700"
+            }}
+          />
+
+          <ChampionSelector filter={filter} selectedChampions={selectedChampions} setSelectedChampions={setSelectedChampions} />
+        </GridItem>
+
+        <GridItem
+          colSpan={2}
+          rowSpan={1}
+        >
+          a
+        </GridItem>
+
+      </Grid>
     </BaseLayout>
   )
 }
