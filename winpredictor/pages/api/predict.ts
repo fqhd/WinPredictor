641fe07c6878fb81eb656dbbea28d5ef7a8522e5
Nemika-Haj <mmnemijus@gmail.com>
2023-05-11 01:05:58 +0300

    chore: create predict api endpoint

--- winpredictor/pages/api/predict.ts
+++ winpredictor/pages/api/predict.ts
@@ -1,11 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as tf from "@tensorflow/tfjs"
+import champVector from "../../src/champVector.json"
 
 type Data = {
   prediction: number
 }
 
-export default function predictionHandler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function predictionHandler(req: NextApiRequest, res: NextApiResponse<Data | string>) {
+  if (req.method != "GET") {
+    return res.status(405).json("method not allowed")
+  }
 
+  const champQuery: string | undefined = req.query.champions as string | undefined
+  if (!champQuery) return res.status(400).json("bad request")
+  const champions = champQuery.replace(" ", "").split(",")
 
-  res.status(200).json({ prediction: 0 })
+  if (champions.length != 10) return res.status(400).json("bad request")
+
+  for (let champion of champions) {
+    if (!(champion in champVector)) {
+      return res.status(400).json(`champion ${champion} not found`)
+    }
+  }
+
+  const model = await tf.loadLayersModel(`http://${req.headers.host}/model/v1.json`);
+
+  let inputs: number[] = [];
+  for (const champ of champions) {
+    // @ts-ignore
+    inputs = inputs.concat(champVector[champ]);
+  }
+
+  const X = tf.tensor([inputs]);
+  const result = model.predict(X);
+
+  // @ts-ignore
+  const prediction = (await result.data()).at(0);
+
+  res.status(200).json({ prediction })
 }
