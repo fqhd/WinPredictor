ecf6d402487f4701acbf0050c8da076391eab760
Nemika-Haj <mmnemijus@gmail.com>
2023-05-11 11:09:02 +0300

    chore: create database for games

---
+++ winpredictor/src/database/core.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import GameModel, { GameType } from "./types/Game";
+
+if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is a required field in .env")
+}
+
+mongoose.connect(process.env.MONGO_URI)
+
+export async function addGame(rank: GameType['rank'], gameUrl: string) {
+    const doc = new GameModel({
+        rank,
+        gameUrl
+    })
+
+    await doc.save()
+}
+
+export async function getAllGames(rank: GameType['rank'] | undefined = undefined, count: boolean = false) {
+    if (!count) {
+        return await GameModel.find(rank ? { rank } : {})
+    }
+
+    return await GameModel.count(rank ? { rank } : {})
+}
