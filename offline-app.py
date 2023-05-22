535fd13a883b8e519d2e97a1f4fe87789b8660df
Fahd <fahd31256@gmail.com>
2023-05-22 23:24:30 +0100

    feat: print roles in Enter Champ message

--- offline-app.py
+++ offline-app.py
@@ -9,24 +9,25 @@ class OfflinePredictor:
 			self.models[rank] = tf.keras.models.load_model(f'models/{rank}.h5')
 		self.df = pd.read_csv('champions.csv')
 
-	def get_champ(self, champs_to_exclude):
-		name = input('Enter champion name:')
+	def get_champ(self, champs_to_exclude, role):
+		name = input(f'Enter {role}:')
 		if not name in self.df.values:
 			print('Champion does not exist')
-			return self.get_champ(champs_to_exclude)
+			return self.get_champ(champs_to_exclude, role)
 		elif name in champs_to_exclude:
 			print('Champion is already taken')
-			return self.get_champ(champs_to_exclude)
+			return self.get_champ(champs_to_exclude, role)
 		return name
 
-	def get_game_champs(self, name):
+	def get_game_champs(self):
 		print(f'Enter champs for blue team')
 		champs = []
+		roles = ['TOP', 'JGL', 'MID', 'ADC', 'SUP']
 		for i in range(5):
-			champs.append(self.get_champ(champs_to_exclude=champs))
+			champs.append(self.get_champ(champs_to_exclude=champs, role=roles[i]))
 		print(f'Enter champs for red team')
 		for i in range(5):
-			champs.append(self.get_champ(champs_to_exclude=champs))
+			champs.append(self.get_champ(champs_to_exclude=champs, role=roles[i]))
 		return champs
 
 	def get_champ_vec(self, champ_name):
@@ -67,7 +68,7 @@ class OfflinePredictor:
 
 	def run(self):
 		rank = self.pick_rank()
-		champs = self.get_game_champs('blue')
+		champs = self.get_game_champs()
 		inputs = []
 		for i in champs:
 			inputs += self.get_champ_vec(i)
